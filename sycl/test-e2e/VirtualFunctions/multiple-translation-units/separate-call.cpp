f6333b973201fcd4eb29da3b3828ff672bb93458
Cai, Justin <justin.cai@intel.com>
2025-02-07 18:22:31 +0000

    tmp

--- sycl/test-e2e/VirtualFunctions/multiple-translation-units/separate-call.cpp
+++ sycl/test-e2e/VirtualFunctions/multiple-translation-units/separate-call.cpp
@@ -1,11 +1,5 @@
 // REQUIRES: aspect-usm_shared_allocations
 //
-// VTables are global variables with possibly external linkage and that causes
-// them to be copied into every module we produce during device code split
-// which in turn leads to multiple definitions error at runtime.
-// XFAIL: run-mode
-// XFAIL-TRACKER: https://github.com/intel/llvm/issues/15069
-//
 // This test covers a scenario where virtual functions defintion and their uses
 // are split into different translation units. In particular:
 // - both virtual functions and construct kernel are in the same translation
